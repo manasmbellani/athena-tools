f5e08d544849c3191ef5dd7bc21da02af5594ebf
Manas Bellani <edwards.aus@hotmail.com>
2020-07-20 19:43:18 +1000

    added or updated new tools or techniques

--- docker/remove_dangling_docker_images.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# 
-# Taken from: 
-#    https://stackoverflow.com/questions/49021954/how-to-remove-dangling-images-in-docker
-sudo docker rmi $(sudo docker images -f "dangling=true" -q)
