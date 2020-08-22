f7d8b77ce2fa548de82650dc2e98f8fbe6ce7575
Manas Bellani <edwards.aus@hotmail.com>
2020-08-22 23:40:30 +1000

    invoke_file_copy_with_progress.sh: updated script to sync files/folders between src and dest

--- mydevice/invoke_file_copy_with_progress.sh
+++ mydevice/invoke_file_copy_with_progress.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
-# 
-# Script copies file from source to destination via one of the following methods:
-#     rsync
-# 
+USAGE=" 
+[-] Usage: $0 <srcfile/srcfolder> <dstfile/dstfolder> [method=rsync]
+
+Description: 
+Script copies file from source to destination via one of the following methods:
+rsync
+
+Parameters:
+    srcfile/srcfolder: Source folder/file to copy across to destination
+    dstfile/dstfolder: Destination folder/file to receive the files
+    method: Method to use to copy the files across. By default, rsync.
+"
+
 if [ $# -lt 2 ]; then
-    echo "[-] $0 <srcfile> <dstfile> [method=rsync]"
+    echo "$USAGE"
     exit 1
 fi
 srcfile="$1"
@@ -12,12 +21,24 @@ dstfile="$2"
 method=${3:-"rsync"}
 
 echo "[*] Checking if file: $srcfile exists"
-if [ ! -f "$srcfile" ]; then
+if [ ! -e "$srcfile" ]; then
     echo "[-] srcfile: $srcfile to copy does not exist"
     exit 1
 fi
 
 if [ "$method" == "rsync" ]; then
-    echo "[*] Copy the file from srcfile: $srcfile to dstfile: $dstfile"
-    rsync -ah --progress "$srcfile" "$dstfile"
+    if [ -f "$srcfile" ]; then
+        echo "[*] Copy the file from srcfile: $srcfile to dstfile: $dstfile"
+        rsync -ah --progress "$srcfile" "$dstfile"
+    elif [ -d "$srcfile" ]; then 
+        echo "[*] Copying files recursively from srcfolder: $srcfile to dstfolder: $dstfile"
+        srcfile=$(echo "$srcfile" | sed -E "s/\/$//g")
+        rsync -arh --progress "$srcfile/" "$dstfile"
+    else
+        echo "[-] Unknown src type: $srcfile"
+        exit 1
+    fi
+else
+    echo "[-] Unknown method: $method"
+    exit 1
 fi
